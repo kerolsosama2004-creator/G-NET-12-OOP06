657b9c25a9ed4f6217f642068158fd6321473924
kerlos <kerlos@DESKTOP-GRP9U8V>
2026-03-14 17:00:55 +0200

    G-NET-12-OOP06 assignment

---
+++ G-NET-12-OOP06/TicketExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_OOP06
+{
+    public static class TicketExtensions
+    {
+        public static string GenerateReceipt(this Ticket ticket)
+        {
+            return
+    $@"========== RECEIPT ==========
+  Movie    : {ticket.Movie}
+  Type     : {ticket.GetType().Name}
+  Price    : {ticket.Price}
+  Final    : {ticket.CalculateFinalPrice():F2}
+  Status   : {(ticket.IsBooked ? "Booked" : "Not Booked")}
+=============================";
+        }
+
+        public static double TotalRevenue(this Ticket[] tickets)
+        {
+            return tickets.Sum(t => t.CalculateFinalPrice());
+        }
+    }
+}
---
+++ G-NET-12-OOP06/VIPTicket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_OOP06
+{
+    internal class VIPTicket : Ticket
+    {
+        public bool LoungeAccess { get; }
+        public double Fee { get; }
+
+        public VIPTicket(int id, string movie, double price, bool loungeAccess, double fee)
+            : base(id, movie, price)
+        {
+            LoungeAccess = loungeAccess;
+            Fee = fee;
+        }
+
+        public override double CalculateFinalPrice()
+        {
+            return (Price + Fee) * 1.14;
+        }
+
+        public override string Category()
+        {
+            return "VIP";
+        }
+    }
+}
